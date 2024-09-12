d8435be2cb7389a115a30bf6a3892558b3759579
Auditikundu <aukundu@deloitte.com>
2024-09-12 14:13:20 +0530

    connection

--- connection.py
+++ connection.py
@@ -3,14 +3,14 @@ import snowflake.connector
 conn = snowflake.connector.connect(
     user= 'AUDITI',
     password= '12qwaszx@12QW',
-    account= 'myaccount',
+    account= 'vj46708.central-india.azure',
     database= 'SAMPLE_NEW'
     )
  
 cur = conn.cursor()
 try:
-    cur.execute("select * from dbo.employee")
-    one_row = cur.fetchone()
+    cur.execute("select * from public.employee")
+    one_row = cur.fetchall()
     print(one_row)
 finally:
     cur.close()
