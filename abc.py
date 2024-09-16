3abb591fd31a2276f84c3f34e733da799be12ab3
Auditikundu <aukundu@deloitte.com>
2024-09-16 17:41:30 +0530

    create a table

--- abc.py
+++
@@ -1,9 +0,0 @@
-print("hello world")
-#Reverse a string
-s = "i.like.this.program.very.much"
-s = s.split(".")
-i,j =  0, len(s)-1
-while(i<=j):
-    s[i],s[j] = s[j],s[i]
-    i,j = i+1, j-1
-print(".".join(s))
